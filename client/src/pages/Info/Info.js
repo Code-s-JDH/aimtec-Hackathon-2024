c108d7f74be3b163a7baf3d961e83fefe885ef1f
DanoHrCZ <daniel.hrecin@gmail.com>
2024-03-09 05:18:06 +0100

    index.css fix

--- client/src/pages/Info/Info.js
+++ client/src/pages/Info/Info.js
@@ -2,9 +2,12 @@ import React from 'react'
 import './info.css'
 
 const Info = () => {
-  return (
-    <div>Info</div>
-  )
+  return <>
+  <div className="border">
+    <h1>Informace</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur minima at error et voluptas ipsa nihil animi laboriosam ullam nam, sed nulla, necessitatibus hic quidem ex quam, optio reiciendis quaerat.</p>
+    </div>
+    </>
 }
 
 export default Info
