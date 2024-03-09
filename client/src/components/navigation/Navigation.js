29a2668eb7786d992d1be89fc430290091a30a2a
DanoHrCZ <daniel.hrecin@gmail.com>
2024-03-09 01:29:37 +0100

    Vytvoření navigace

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { faLanguage, faInfo, faHashtag, faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons'
 import IconBtn from '../../ui/buttons/iconBtn/iconBtn'
+import './navigation.css'
 
 const Navigation = () => {
   return (
-    <nav>
-      <div>
+    <nav >
+      <div className='links'>
         <NavLink to="/">
           <IconBtn icon={faLanguage} />
         </NavLink>
         <NavLink to="/info">
-          <IconBtn icon={faInfo} />
+          <IconBtn   icon={faInfo} />
         </NavLink>
         <NavLink to="/blog">
           <IconBtn icon={faHashtag} />
