67df600b360ede91587579b0f665ab04de24343d
DanoHrCZ <daniel.hrecin@gmail.com>
2024-03-09 06:25:42 +0100

    přidání loga

--- client/src/layouts/AppLayout.js
+++ client/src/layouts/AppLayout.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Navigation from '../components/navigation/Navigation'
 import "./appLayout.css"
+import logo from "../media/sprites/logo.png"
 
 const AppLayout = () => {
   return (<div id='mainWrapper'>
+    <img className="image_logo" src={logo} alt="" />
     <Outlet></Outlet>
     <Navigation />
   </div>
