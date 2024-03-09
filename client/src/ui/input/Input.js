1e3288514f9bef878b31a8c948e279063249431d
DanoHrCZ <daniel.hrecin@gmail.com>
2024-03-09 02:44:44 +0100

    vytvoření Ui komponentu "Input"

---
+++ client/src/ui/input/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Input.css";
+
+const Input = () =>{
+    return(
+        <div className="input">
+            <input type="text" placeholder="enter text"/>
+        </div>
+    )
+}
+export default Input;
