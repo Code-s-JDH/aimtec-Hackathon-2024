0575fa601c5d8c495312ce4471c85ac235ef3c3f
JirikPos <Jirikpos221@gmail.com>
2024-03-09 04:44:44 +0100

    added audio service

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import './home.css'
+import React, { useState } from 'react';
+import { startRecording, stopRecording } from '../../service/audioService';
+import { textToSpeech, speechToText, textToSymbols, symbolsToText } from '../../service/translatorApi';
+
+import './home.css';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [speechBase64, setSpeechBase64] = useState('');
+  const [resultText, setResultText] = useState('');
 
-export default Home
+  const handleStartRecording = async () => {
+    try {
+      const base64data = await startRecording();
+      setSpeechBase64(base64data);
+    } catch (error) {
+      console.error('Chyba při nahrávání zvuku:', error);
+    }
+  };
+
+  const handleSpeechToText = async () => {
+    try {
+      const textData = await speechToText(speechBase64);
+      setResultText(textData);
+    } catch (error) {
+      console.error('Chyba při konverzi řeči na text:', error);
+    }
+  };
+
+  const handleTextToSpeech = async () => {
+    try {
+      const speechData = await textToSpeech('Hello, this is a test.');
+    } catch (error) {
+      console.error('Chyba při konverzi textu na řeč:', error);
+    }
+  };
+
+  return (
+    <div></div>
+  );
+};
+
+export default Home;
--- client/src/service/translatorApi.js
+++ client/src/service/translatorApi.js
@@ -1,31 +1,58 @@
 import axios from '../api/axios';
 import config from '../config/config';
 
-const textToSpeech = async () => {
+const textToSpeech = async (text, textLanguage, textToLanguage) => {
   try {
-    const response = await axios.post(`${config.apiUrl}/`);
-    return response.data;
+    const response = await axios.post(`${config.apiUrl}/TextToSpeech`, {
+      text,
+      textLanguage,
+      textToLanguage,
+    });
+
+    return response.data.speech;
   } catch (error) {
     throw error;
   }
 };
 
-const speechToText = async () => {
+const speechToText = async (speech, speechLanguage, speechToLanguage) => {
   try {
-    const response = await axios.post(`${config.apiUrl}/`);
-    return response.data;
+    const response = await axios.post(`${config.apiUrl}/SpeachToText`, {
+      speech,
+      speechLanguage,
+      speechToLanguage,
+    });
+
+    return response.data.text;
   } catch (error) {
     throw error;
   }
 };
 
-const symbolsToText = async () => {
+const symbolsToText = async (image) => {
   try {
-    const response = await axios.post(`${config.apiUrl}/`);
-    return response.data;
+    const response = await axios.post(`${config.apiUrl}/SymbolsToText`, {
+      image,
+    });
+
+    return response.data.text;
   } catch (error) {
     throw error;
   }
 };
 
-export { textToSpeech, speechToText, symbolsToText };
+const textToSymbols = async (text, textLanguage, textToLanguage) => {
+  try {
+    const response = await axios.post(`${config.apiUrl}/TextToSymbols`, {
+      text,
+      textLanguage,
+      textToLanguage,
+    });
+
+    return response.data.image;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { textToSpeech, speechToText, symbolsToText, textToSymbols };
